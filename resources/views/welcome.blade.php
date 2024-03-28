1213a2eefbb8f1caa15365b75e58f14465309393
Momen <132298128+Mano4life@users.noreply.github.com>
2024-03-28 20:03:21 +0200

    changed accent on to top btn and adjusted welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,16 +30,21 @@
 </head>
 
 <body>
+
     <x-navbar />
     <x-hero />
+
   <main id="main">
+
     <x-about />
     <x-counts />
     <x-services />
     <x-projects />
     <x-team />
     <x-contact />
+
   </main><!-- End #main -->
+  
     <x-footer />
 
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
