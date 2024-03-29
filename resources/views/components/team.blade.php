8316b2dad9e43f94a8adc49ea8cf3f117aac3629
abdullrahman <am6151342@gmail.com>
2024-03-30 01:11:36 +0200

    teams and services section

--- resources/views/components/team.blade.php
+++ resources/views/components/team.blade.php
@@ -1,6 +1,6 @@
     <!-- ======= Our Team Section ======= -->
     <section id="team" class="team">
-      <div class="container">
+      {{-- <div class="container">
 
         <div class="section-title">
           <h2>Our Team</h2>
@@ -75,5 +75,115 @@
 
         </div>
 
+      </div> --}}
+
+      <div class="teams">
+        <div class="oval-cards">
+          <div class="card">
+            <h2>Momen</h2>
+            <img src="assets/img/team/momen.jfif" alt="">
+            <h3>web devlpoer</h3>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, officia?</h3>
+            
+          </div>
+          
+          <div class="card">
+            <h2>Mustafa</h2>
+            <img src="assets/img/team/mostafa.jfif" alt="">
+            <h3>web devlpoer</h3>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, officia?</h3>
+            
+          </div>
+          
+          
+          <div class="card">
+            <h2>AbdElrahman</h2>
+            <img src="assets/img/team/abdo.png" alt="">
+            <h3>web devlpoer</h3>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, officia?</h3>
+
+          </div>
+
+          <div class="card">
+            <h2>Zeyad</h2>
+            <img src="assets/img/team/zeyad.png" alt="">
+            <h3>web devlpoer</h3>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, officia?</h3>
+          </div>
+        </div>
       </div>
+
     </section><!-- End Our Team Section -->
+
+
+    <style>
+/* .teams{
+  display: flex;
+  width: 100%;
+    } */
+.teams .oval-cards{
+  height: 30rem;display: flex;
+  flex-direction: row;
+  gap: 40px;
+
+  justify-content: space-around;
+}
+     .teams .oval-cards .card {
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  width:25%;
+  background: #000;
+  height: 50%;
+  transition: .3s;
+  border-radius: 6px ;
+  box-shadow: 5px 5px 5px black;
+}
+.teams .oval-cards .card:hover{
+  scale: 1.1;
+  cursor: pointer;
+  background: #fff;
+}
+
+.teams .oval-cards .card:hover img{
+  border: 2px solid #7c3aed;
+}
+.teams .oval-cards .card:hover h2,.teams .oval-cards .card:hover h3{
+  color: #000;
+}
+      .teams .oval-cards .card h2{
+        padding: 3rem 2rem 0 0;
+        font-family: 'Courier New', Courier, monospace;
+        color: #fff;
+        font: 1rem;
+        font-weight: bold;
+        text-align: center;
+      }
+      .teams .oval-cards .card h3{
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        padding:0 0 0 2rem  ;
+        text-align: left;
+        color: #fff;
+        /* color: #7c3aed; */
+        font-weight: 500;
+        letter-spacing: .2px;
+        font-size: 1.5rem;
+        
+      }
+
+      .teams .oval-cards .card img{
+        position: absolute;
+        transition: .2s;
+  top: -55px;
+  object-fit: cover;
+  object-position:top;
+ 
+  left: 50%;
+  transform: translateX(-50%);
+  width: 30%;
+  height: 100px;
+  border-radius: 50%  ;
+      }
+    </style>
+
+
