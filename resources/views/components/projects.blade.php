27686de307377b554ef3638ae0bf9580de374c82
Momen <132298128+Mano4life@users.noreply.github.com>
2024-03-31 04:38:23 +0200

    added target blank to projects links

--- resources/views/components/projects.blade.php
+++ resources/views/components/projects.blade.php
@@ -23,8 +23,8 @@
             <h1 class="title">EgTour</h1>
 
             <div class="character two">
-              <a href="https://github.com/abdullrahman-2/eg-tour"><i class="ri-github-fill project-icon"></i></a>
-              <a href="assets/img/portfolio/image.jpg"><i class="ri-image-fill project-icon"></i></a>
+              <a href="https://github.com/abdullrahman-2/eg-tour" target="_blank" ><i class="ri-github-fill project-icon"></i></a>
+              <a href="assets/img/portfolio/image.jpg" target="_blank" ><i class="ri-image-fill project-icon"></i></a>
             </div>
           </div>
       
@@ -36,8 +36,8 @@
             <h1 class="title">ElKhabeer</h1>
 
             <div class="character two">
-              <a href="https://github.com/Zeyad-2004/al-khabeer"><i class="ri-github-fill project-icon"></i></a>
-              <a href="assets/img/portfolio/photo2.jpg"><i class="ri-image-fill project-icon"></i></a>
+              <a href="https://github.com/Zeyad-2004/al-khabeer" target="_blank" ><i class="ri-github-fill project-icon"></i></a>
+              <a href="assets/img/portfolio/photo2.jpg" target="_blank" ><i class="ri-image-fill project-icon"></i></a>
             </div>
           </div>
 
@@ -49,8 +49,8 @@
             <h1 class="title">Brandy's Price List</h1>
 
             <div class="character two">
-              <a href="https://github.com/shks33/General"><i class="ri-github-fill project-icon"></i></a>
-              <a href="assets/img/portfolio/image3.png"><i class="ri-image-fill project-icon"></i></a>
+              <a href="https://github.com/shks33/General" target="_blank" ><i class="ri-github-fill project-icon"></i></a>
+              <a href="assets/img/portfolio/image3.png" target="_blank" ><i class="ri-image-fill project-icon"></i></a>
             </div>
           </div>
 
@@ -62,8 +62,8 @@
             <h1 class="title">WildLife</h1>
 
             <div class="character two">
-              <a href="https://github.com/Mano4life/in-out-carousel?tab=readme-ov-file"><i class="ri-github-fill project-icon"></i></a>
-              <a href="assets/img/portfolio/image4.jpg"><i class="ri-image-fill project-icon"></i></a>
+              <a href="https://github.com/Mano4life/in-out-carousel?tab=readme-ov-file" target="_blank" ><i class="ri-github-fill project-icon"></i></a>
+              <a href="assets/img/portfolio/image4.jpg" target="_blank" ><i class="ri-image-fill project-icon"></i></a>
             </div>
           </div>
 
